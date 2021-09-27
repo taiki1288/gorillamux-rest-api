2d2c9a33b3f5311d116ca68d387cc9bb61c65322
taiki yamashita <taikity0808@gmail.com>
2021-09-27 17:07:45 +0900

    feature: fmtパッケージをインポート

--- server.go
+++ server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	
+	router := mux.NewRouter();
+	router.HandleFunc("/", func(resp http.ResponseWriter, req *http.Request) {
+		fmt.Println(resp, "Up and running")
+	})
 }
