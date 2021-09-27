0ccc3606b1690c12789e1ce4c8b5c774dd956cf5
taiki yamashita <taikity0808@gmail.com>
2021-09-27 18:46:23 +0900

    feature: init関数を作成

--- route.go
+++ route.go
@@ -9,3 +9,11 @@ type Post struct {
 	Title string `json:"title"`
 	Text  string `json:"text"`
 }
+
+var (
+	posts []Post
+)
+
+func init() {
+	posts = []Post{Post{Id: 1, Title: "Title 1", Text: "Text 1"}}
+}
