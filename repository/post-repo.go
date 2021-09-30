8998a0cef962c82af76359c07f21a1e4d081fc44
taiki yamashita <taikity0808@gmail.com>
2021-10-01 00:04:09 +0900

    feature: FindAll関数を作成

--- repository/post-repo.go
+++ repository/post-repo.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"context"
 	"log"
+	"strings"
 
 	"../entity"
+	"google.golang.org/api/iterator"
 
 	"cloud.google.com/go/firestore"
 )
@@ -47,4 +49,32 @@ func (*repo) Save(post *entity.Post) (*entity.Post, error) {
 	}
 
 	return post, nil
+}
+
+func (*repo) FindAll() ([]entity.Post, error) {
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectId)
+	if err != nil {
+		log.Fatalf("Failed to create a Firestore Client: %v", err)
+		return nil, err
+	}
+	defer client.Close()
+
+	var posts []entity.Post
+	
+	iterator := client.Collection(collectionName).Documents(ctx)
+	for {
+		doc, err := iterator.Next()
+		if err != nil {
+			log.Fatalf("Failed to iterate the list of posts: %v", err)
+		    return nil, err
+		}
+		post := entity.Post {
+			ID: doc.Data()["ID"].(int),
+			Title: doc.Data()["Title"].(string),
+			Text: doc.Data()["Text"].(string),
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
 }
